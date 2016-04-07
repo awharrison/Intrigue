5908f46a80d1a75bd247257184ce568cc31b49de
Abby Harrison <awharrison@ABBYSMACPRO.WIFI.WPI.EDU>
2016-04-06 20:20:57 -0400

    added initialize and dimension to game

--- src/game/Intrigue.java
+++ src/game/Intrigue.java
@@ -1,5 +1,7 @@
 package game;
 
+import java.awt.Dimension;
+
 import ks.common.games.Solitaire;
 import ks.common.model.Column;
 import ks.common.model.MultiDeck;
@@ -30,5 +32,17 @@ public class Intrigue extends Solitaire {
 	@Override
 	public boolean hasWon() {
 		return false; // TODO determine what a win is
+	}
+	
+	@Override
+	public void initialize() {
+		// initialize model
+		// TODO this will be a pretty large part of the game
+		
+	}
+
+	@Override
+	public Dimension getPreferredSize() {
+		return new Dimension (800, 300);
 	}
 }
